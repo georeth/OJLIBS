72a15e1d67eeb9d285a5075a8178740aa732109c
Georeth Chow <georeth2010@gmail.com>
2016-10-03 02:40:35 +0800

    add missing namespaces to disjoint_set.hpp

--- inc/ojlibs/disjoint_set.hpp
+++ inc/ojlibs/disjoint_set.hpp
@@ -3,6 +3,9 @@
 
 #include <vector>
 
+namespace ojlibs { // TO_BE_REMOVED
+namespace data_structure { // TO_BE_REMOVED
+
 struct disjoint_set {
     std::vector<int> parent;
     void reset(int size){
@@ -11,23 +14,22 @@ struct disjoint_set {
             parent[i] = i;
         }
     }
-    disjoint_set(int size){
-        reset(size);
-    }
+    disjoint_set(int size){ reset(size); }
     int find(int c){
         int pc = parent[c];
         return (pc == c) ? c : (parent[c] = find(pc));
     }
     // ancestor(p) is parent of ancestor(c)
     bool union_if(int p, int c){
-        int pp = find(p);
-        int pc = find(c);
-        if (pp == pc)
-            return false;
+        int pp = find(p), pc = find(c);
+        if (pp == pc) return false;
         parent[pc] = pp;
         return true;
     }
 };
+
+} // data_structure TO_BE_REMOVED
+} // ojlibs TO_BE_REMOVED
 
 
 #endif /* end of include guard: OJLIBS_INC_DISJOINT_SET_H_ */
