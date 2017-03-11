e1080d6cfb50c05feb5a63d9328210f77a32e862
georeth <georeth2010@gmail.com>
2017-03-11 22:15:59 +0800

    add 2-sat

---
+++ inc/ojlibs/2sat.hpp
@@ -0,0 +1,41 @@
+#ifndef OJLIBS_INC_2SAT_H_
+#define OJLIBS_INC_2SAT_H_
+
+#include <cassert>
+#include <vector>
+#include <algorithm>
+#include <ojlibs/scc_tarjan.hpp>
+
+namespace ojlibs { // TO_BE_REMOVED
+    template <typename EInfo>
+    bool find_2sat(graph<EInfo> &g, std::vector<bool> &value_out) {
+        assert(g.vertex_size() % 2 == 0);
+        int nr_var = g.vertex_size() / 2;
+
+        int nr_scc;
+        std::vector<int> scc_id = scc_tarjan(g, nr_scc);
+
+        std::vector<bool> assigned(g.vertex_size());
+        std::vector<std::vector<int>> scc(nr_scc);
+
+        for (int i = 0; i < nr_var; ++i) {
+            if (scc_id[2 * i] == scc_id[2 * i + 1]) return false;
+        }
+
+        vector<bool> value(nr_var);
+        for (int i = 0; i < 2 * nr_var; ++i)
+            scc[scc_id[i]].push_back(i);
+
+        for (int i = 0; i < nr_scc; ++i) {
+            for (int u : scc[i]) {
+                if (!assigned[u / 2]) {
+                    assigned[u / 2] = true;
+                    value[u / 2] = (u + 1) % 2;
+                }
+            }
+        }
+        value_out = value;
+        return true;
+    }
+} // namespace ojlibs TO_BE_REMOVED
+#endif /* end of include guard: OJLIBS_INC_2SAT_H_ */
