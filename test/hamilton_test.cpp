f32616208c3f3992cc055698e9de7e1e40278477
Georeth Chow <georeth2010@gmail.com>
2020-06-02 20:55:42 +0800

    add Hamiltonian cycle finding algorithm

---
+++ test/hamilton_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <random>
+#include <ojlibs/hamilton.hpp>
+
+using namespace std;
+using uni = std::uniform_int_distribution<>;
+std::mt19937 gen;
+
+TEST(BASIC, ctor) {
+}
