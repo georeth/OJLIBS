2328e6dfb3f084e0387a5fd6d8b871df4b263773
Georeth Chow <georeth2010@gmail.com>
2021-01-02 20:03:04 +0800

    refactor matrix: ring_traits -> algebra

--- inc/ojlibs/matrix.hpp
+++ inc/ojlibs/matrix.hpp
@@ -3,75 +3,26 @@
 #include <vector>
 #include <cmath>
 #include <cassert>
-#include <ojlibs/arith.hpp>
+#include <ojlibs/algebra/base.hpp>
 
 // This library  supports common operations in linear algebra
 // for different rings (fields).
 
 namespace ojlibs { // TO_BE_REMOVED
 
-// TYPES
-template <typename T>
-struct ring_traits {
-    // using defualt operator==
-    static T zero() { return T(); }
-    static T negate(const T &p) { return -p; }
-    static T plus(const T &p1, const T &p2) { return p1 + p2; }
-    static T minus(const T &p1, const T &p2) { return p1 - p2; }
-    static T one() { return 1; }
-    static T time(const T &p1, const T &p2) { return p1 * p2; }
+#define TEMPL_ARG template <typename Alg>
+#define EleType typename Alg::type
+#define MAT_T matrix<Alg>
 
-    static void plus_assign(T &p1, const T &p2) { p1 += p2; }
-    static void minus_assign(T &p1, const T &p2) { p1 -= p2; }
-    static void time_assign(T &p1, const T &p2) { p1 *= p2; }
-
-    // optional
-    static T inverse(const T &p) { return 1 / p; }
-    static T divide(const T &p1, const T &p2) { return p1 / p2; }
-
-    static void divide_assign(T &p1, const T &p2) { p1 /= p2; }
-
-    // norm >= 0
-    // norm == 0 <=> p == zero
-    static double norm(const T &p) { return std::abs(p); }
-};
-
-template <typename Int = int, Int MOD = 1000000007>
-struct modular_ring {
-    // using defualt operator==
-    static Int zero() { return 0; }
-    static Int negate(const Int &p) { return subx_mod(0, p, MOD); }
-    static Int plus(const Int &p1, const Int &p2) { return addx_mod(p1, p2, MOD); }
-    static Int minus(const Int &p1, const Int &p2) { return subx_mod(p1, p2, MOD); }
-    static Int one() { return 1; }
-    static Int time(const Int &p1, const Int &p2) { return mul_mod(p1, p2, MOD); }
-
-    static void plus_assign(Int &p1, const Int &p2) { p1 = plus(p1, p2); }
-    static void minus_assign(Int &p1, const Int &p2) { p1 = minus(p1, p2); }
-    static void time_assign(Int &p1, const Int &p2) { p1 = time(p1, p2); }
-
-    // optional
-    static Int inverse(const Int &p) { return inv_mod(p, MOD); }
-    static Int divide(const Int &p1, const Int &p2) { return time(p1, inverse(p2)); }
-
-    static void divide_assign(Int &p1, const Int &p2) { p1 = divide(p1, p2); }
-
-    // norm >= 0
-    // norm == 0 <=> p == zero
-    static double norm(const Int &p) { return p; }
-};
-
-#define TEMPL_ARG template <typename T, typename EleTraits>
-#define TEMPL_ARG_DEFAULT \
-    template <typename T, typename EleTraits = ring_traits<T>>
-#define MAT_T matrix<T, EleTraits>
-
-TEMPL_ARG_DEFAULT
+TEMPL_ARG
 struct matrix {
     int nrow, ncol;
+    using T = typename Alg::type;
     std::vector<T> arr;
-    explicit matrix(int r = 0, int c = 0, const T &init = EleTraits::zero())
-        : nrow(r), ncol(c), arr(nrow * ncol, init) { }
+    Alg alg;
+
+    explicit matrix(int r = 0, int c = 0, const T &init = 0, const Alg &alg_in = Alg())
+        : nrow(r), ncol(c), alg(alg_in), arr(nrow * ncol, init) { }
     bool operator==(const matrix &that) const {
         assert(nrow == that.nrow && ncol == that.ncol);
         for (int i = 0; i < nrow * ncol; ++i)
@@ -82,10 +33,10 @@ struct matrix {
     int c() const { return ncol; }
     T &operator()(int i, int j) { return arr[i * ncol + j]; }
     const T &operator()(int i, int j) const { return arr[i * ncol + j]; }
-    static matrix eye(int n) {
+    static matrix eye(int n, const Alg &alg = Alg()) {
         matrix mat(n, n);
         for (int i = 0; i < n; ++i)
-            mat(i, i) = EleTraits::one();
+            mat(i, i) = alg.one();
         return mat;
     }
 };
@@ -96,15 +47,14 @@ MAT_T operator*(const MAT_T &left, const MAT_T &right) {
     int mm = left.r(), nn = left.c(), pp = right.c();
     assert(nn == right.r());
 
-    EleTraits traits;
-
+    auto &alg = left.alg;
     MAT_T ret(mm, pp);
     for (int i = 0; i < mm; ++i) {
         for (int j = 0; j < pp; ++j) {
-            T acc = traits.zero();
+            auto acc = alg.zero();
             for (int k = 0; k < nn; ++k)
-                traits.plus_assign(acc,
-                        traits.time(left(i, k), right(k, j)));
+                acc = alg.plus(acc,
+                        alg.multiplies(left(i, k), right(k, j)));
             ret(i, j) = acc;
         }
     }
@@ -118,10 +68,10 @@ MAT_T &operator op_eq_str(MAT_T &left, const MAT_T &right) { \
     int rr = left.r(), cc = left.c(); \
     assert(rr == right.r() && cc == right.c()); \
  \
-    EleTraits traits; \
+    auto &alg = left.alg; \
     for (int i = 0; i < rr; ++i) \
         for (int j = 0; j < cc; ++j) \
-            traits.op_name##_assign(left(i, j), right(i, j)); \
+            left(i, j) = alg.op_name(left(i, j), right(i, j)); \
     return left; \
 } \
 TEMPL_ARG \
@@ -139,59 +89,58 @@ MAT_T operator-(const MAT_T &m) {
     int cc = m.c();
 
     MAT_T ret(m);
-    EleTraits traits;
+    auto &alg = m.alg;
     for (int i = 0; i < rr; ++i)
         for (int j = 0; j < cc; ++j)
-            ret(i, j) = traits.negate(m(i, j));
+            ret(i, j) = alg.negate(m(i, j));
     return ret;
 }
 #define MATRIX_SCALAR_OP_GEN(op_str, op_eq_str, op_name) \
 TEMPL_ARG \
-MAT_T &operator op_eq_str(MAT_T &left, const T &val) { \
+MAT_T &operator op_eq_str(MAT_T &left, const EleType &val) { \
     int rr = left.r(); \
     int cc = left.c(); \
-    EleTraits traits; \
+    auto &alg = left.alg; \
     for (int i = 0; i < rr; ++i) { \
         for (int j = 0; j < cc; ++j) { \
-            traits.op_name##_assign(left(i, j), val); \
+            left(i, j) = alg.op_name(left(i, j), val); \
         } \
     } \
     return left; \
 } \
 TEMPL_ARG \
-MAT_T operator op_str(const MAT_T &left, const T &val) { \
+MAT_T operator op_str(const MAT_T &left, const EleType &val) { \
     MAT_T ret(left); \
     return ret op_eq_str val; \
 } \
 TEMPL_ARG \
-MAT_T &operator op_eq_str(const T &val, MAT_T &right) { \
+MAT_T &operator op_eq_str(const EleType &val, MAT_T &right) { \
     int rr = right.r(); \
     int cc = right.c(); \
-    EleTraits traits; \
+    auto &alg = right.alg; \
     for (int i = 0; i < rr; ++i) \
         for (int j = 0; j < cc; ++j) \
-            right(i, j) = traits.op_name(val, right(i, j)); \
+            right(i, j) = alg.op_name(val, right(i, j)); \
     return right; \
 } \
 TEMPL_ARG \
-MAT_T operator op_str(const T &val, const MAT_T &right) { \
+MAT_T operator op_str(const EleType &val, const MAT_T &right) { \
     MAT_T ret(right); \
     return val op_eq_str ret; \
 }
 MATRIX_SCALAR_OP_GEN(+, +=, plus)
 MATRIX_SCALAR_OP_GEN(-, -=, minus)
-MATRIX_SCALAR_OP_GEN(*, *=, time)
-MATRIX_SCALAR_OP_GEN(/, /=, divide)
+MATRIX_SCALAR_OP_GEN(*, *=, multiplies)
+MATRIX_SCALAR_OP_GEN(/, /=, divides)
 #undef MATRIX_SCALAR_OP_GEN
 
 // HELPERS
-TEMPL_ARG_DEFAULT
-MAT_T permutation_matrix(const std::vector<int> &p) {
+TEMPL_ARG
+MAT_T permutation_matrix(const std::vector<int> &p, const Alg &alg = Alg()) {
     int n = (int)p.size();
-    EleTraits traits;
     MAT_T P(n, n);
     for (int i = 0; i < n; ++i)
-        P(i, p[i]) = traits.one();
+        P(i, p[i]) = alg.one();
     return P;
 }
 
@@ -203,21 +152,21 @@ MAT_T permutation_matrix(const std::vector<int> &p) {
 TEMPL_ARG
 int LU_decompose(const MAT_T &A, MAT_T &L, MAT_T &U) {
     int n = A.r();
-    EleTraits traits;
+    auto &alg = A.alg;
     assert(n == A.c());
 
     L = MAT_T::eye(n);
     U = A;
     for (int j = 0; j < n; ++j) {
-        if (U(j, j) == traits.zero())
+        if (U(j, j) == alg.zero())
             return -1;
         for (int i = j + 1; i < n; ++i) {
-            T lij = traits.divide(U(i, j), U(j, j));
+            auto lij = alg.divides(U(i, j), U(j, j));
             L(i, j) = lij;
             // U[i] -= U[j] * lij
-            U(i, j) = traits.zero();
+            U(i, j) = alg.zero();
             for (int k = j + 1; k < n; ++k)
-                traits.minus_assign(U(i, k), U(j, k) * lij);
+                U(i, k) = alg.minus(U(i, k), alg.multiplies(U(j, k), lij));
         }
     }
     return 0;
@@ -232,7 +181,7 @@ TEMPL_ARG
 int PLU_decompose(const MAT_T &A, std::vector<int> &p,
                   MAT_T &L, MAT_T &U, bool &positive) {
     int n = A.r();
-    EleTraits traits;
+    auto &alg = A.alg;
     assert(n == A.c());
 
     positive = true;
@@ -243,18 +192,18 @@ int PLU_decompose(const MAT_T &A, std::vector<int> &p,
         p[i] = i;
 
     for (int j = 0; j < n; ++j) {
-        double max_norm = traits.norm(U(j, j));
+        auto max_norm = alg.norm(U(j, j));
         int max_index = j;
 
         // find largest pivot
         for (int jj = j + 1; jj < n; ++jj) {
-            double cur_norm = traits.norm(U(jj, j));
+            auto cur_norm = alg.norm(U(jj, j));
             if (cur_norm > max_norm) {
                 max_norm = cur_norm;
                 max_index = jj;
             }
         }
-        if (U(max_index, j) == traits.zero())
+        if (U(max_index, j) == alg.zero())
             return -1;
         if (j != max_index) {
             for (int k = 0; k < n; ++k) {
@@ -266,12 +215,12 @@ int PLU_decompose(const MAT_T &A, std::vector<int> &p,
         }
         // as before
         for (int i = j + 1; i < n; ++i) {
-            T lij = traits.divide(U(i, j), U(j, j));
+            auto lij = alg.divides(U(i, j), U(j, j));
             L(i, j) = lij;
             // U[i] -= U[j] * lij
             U(i, j) = 0;
             for (int k = j + 1; k < n; ++k)
-                traits.minus_assign(U(i, k), traits.time(U(j, k), lij));
+                U(i, k) = alg.minus(U(i, k), alg.multiplies(U(j, k), lij));
         }
     }
     for (int i = 0; i < n; ++i)
@@ -280,22 +229,21 @@ int PLU_decompose(const MAT_T &A, std::vector<int> &p,
 }
 
 TEMPL_ARG
-T determinant(const MAT_T &A) {
+EleType determinant(const MAT_T &A) {
     assert(A.r() == A.c());
-
-    EleTraits traits;
+    auto &alg = A.alg;
     std::vector<int> p;
     bool pos;
     MAT_T L, U;
 
     if (PLU_decompose(A, p, L, U, pos) < 0)
-        return traits.zero();
+        return alg.zero();
 
-    T ret = traits.one();
+    auto ret = alg.one();
     for (int i = 0; i < A.r(); ++i)
-        traits.time_assign(ret, U(i, i));
+        ret = alg.multiplies(ret, U(i, i));
     if (!pos)
-        ret = traits.negate(ret);
+        ret = alg.negate(ret);
     return ret;
 }
 
@@ -303,7 +251,7 @@ T determinant(const MAT_T &A) {
 TEMPL_ARG
 MAT_T inverse(const MAT_T &A) {
     assert(A.r() == A.c());
-    EleTraits traits;
+    auto &alg = A.alg;
 
     MAT_T U = A;
     int n = U.r();
@@ -311,18 +259,18 @@ MAT_T inverse(const MAT_T &A) {
 
     // first make U upper right
     for (int j = 0; j < n; ++j) {
-        double max_norm = traits.norm(U(j, j));
+        auto max_norm = alg.norm(U(j, j));
         int max_index = j;
 
         // find largest pivot
         for (int jj = j + 1; jj < n; ++jj) {
-            double cur_norm = traits.norm(U(jj, j));
+            auto cur_norm = alg.norm(U(jj, j));
             if (cur_norm > max_norm) {
                 max_norm = cur_norm;
                 max_index = jj;
             }
         }
-        assert(U(max_index, j) != traits.zero());
+        assert(U(max_index, j) != alg.zero());
 
         if (j != max_index) {
             for (int k = 0; k < n; ++k) {
@@ -332,35 +280,35 @@ MAT_T inverse(const MAT_T &A) {
         }
 
         for (int i = j + 1; i < n; ++i) {
-            T lij = traits.divide(U(i, j), U(j, j));
-            U(i, j) = traits.zero();
+            auto lij = alg.divides(U(i, j), U(j, j));
+            U(i, j) = alg.zero();
             for (int k = j + 1; k < n; ++k)
-                traits.minus_assign(U(i, k), traits.time(U(j, k), lij));
+                U(i, k) = alg.minus(U(i, k), alg.multiplies(U(j, k), lij));
             for (int k = 0; k < n; ++k)
-                traits.minus_assign(R(i, k), traits.time(R(j, k), lij));
+                R(i, k) = alg.minus(R(i, k), alg.multiplies(R(j, k), lij));
         }
     }
 
     // then, make U diagonal
     for (int j = n - 1; j >= 0; --j) {
         for (int i = 0; i < j; ++i) {
-            T lij = traits.divide(U(i, j), U(j, j));
+            auto lij = alg.divides(U(i, j), U(j, j));
 
             for (int k = 0; k < n; ++k)
-                traits.minus_assign(R(i, k), traits.time(R(j, k), lij));
+                R(i, k) = alg.minus(R(i, k), alg.multiplies(R(j, k), lij));
         }
     }
 
     // finally, make U identity
     for (int i = 0; i < n; ++i)
         for (int k = 0; k < n; ++k)
-            R(i, k) = traits.divide(R(i, k), U(i, i));
+            R(i, k) = alg.divides(R(i, k), U(i, i));
 
     return R;
 }
 
 #undef TEMPL_ARG
-#undef TEMPL_ARG_DEFAULT
+#undef EleType
 #undef MAT_T
 
 } // namespace ojlibs TO_BE_REMOVED
--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -3,12 +3,14 @@
 #include <random>
 #include <ojlibs/matrix.hpp>
 #include <ojlibs/power.hpp>
+#include <ojlibs/algebra/modular.hpp>
 using namespace std;
 
-typedef ojlibs::matrix<double> mat_t;
+using Alg = ojlibs::base_algebra<double>;
+typedef ojlibs::matrix<Alg> mat_t;
 
 template <typename T, typename Ring>
-void print(const ojlibs::matrix<T, Ring> &m) {
+void print(const mat_t &m) {
     for (int i = 0; i < m.r(); ++i) {
         for (int j = 0; j < m.r(); ++j)
             cout << m(i, j) << " ";
@@ -18,8 +20,8 @@ void print(const ojlibs::matrix<T, Ring> &m) {
 }
 
 TEST(OP, MM) {
-    ojlibs::matrix<double> m(3, 3, 1.0);
-    ojlibs::matrix<double> m2 = m * m;
+    mat_t m(3, 3, 1.0);
+    mat_t m2 = m * m;
     m + m;
     m - m;
     m += m;
@@ -31,20 +33,22 @@ TEST(OP, MM) {
 }
 
 TEST(HELLO, power) {
-    ojlibs::matrix<int> m(2, 2);
+    using mat_t = ojlibs::matrix<ojlibs::base_algebra<int>>;
+    mat_t m(2, 2);
     m(1, 0) = 1;
     m(1, 1) = 1;
     m(0, 1) = 1;
 
-    ojlibs::matrix<int> m1 = ojlibs::power(m, 6, ojlibs::matrix<int>::eye(2));
-    ojlibs::matrix<int> m2 = m * m * m * m * m * m;
+    mat_t m1 = ojlibs::power(m, 6, mat_t::eye(2));
+    mat_t m2 = m * m * m * m * m * m;
 
     assert(m1 == m2);
 }
 
 TEST(BASIC, operator) {
-    auto m1 = ojlibs::matrix<int>::eye(3);
-    auto m2 = ojlibs::matrix<int>::eye(3);
+    using mat_t = ojlibs::matrix<ojlibs::base_algebra<int>>;
+    auto m1 = mat_t::eye(3);
+    auto m2 = mat_t::eye(3);
     m1 + 3;
     3 + m1;
     m1 - 3;
@@ -66,12 +70,13 @@ TEST(BASIC, operator) {
 
 
 TEST(MULT, random) {
+    using mat_t = ojlibs::matrix<ojlibs::base_algebra<int>>;
     std::uniform_int_distribution<> dis(-100, 100);
     std::mt19937 gen;
 
     const int TEST_SIZE = 20;
     for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
-        ojlibs::matrix<int> m1(10, 200), m2(200, 100), m3(100, 20);
+        mat_t m1(10, 200), m2(200, 100), m3(100, 20);
         for (int i = 0; i < 10; ++i) {
             for (int j = 0; j < 20; ++j) {
                 m1(i, j) = dis(gen);
@@ -90,12 +95,12 @@ TEST(LU, random) {
     std::mt19937 gen;
     
     for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
-        ojlibs::matrix<double> A(M, M);
+        mat_t A(M, M);
         for (int i = 0; i < M; ++i)
             for (int j = 0; j < M; ++j)
                 A(i, j) = dis(gen);
 
-        ojlibs::matrix<double> L, U, LU;
+        mat_t L, U, LU;
         ASSERT_EQ(0, LU_decompose(A, L, U));
         LU = L * U;
         for (int i = 0; i < M; ++i)
@@ -111,14 +116,14 @@ TEST(PLU, random) {
     std::mt19937 gen;
     
     for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
-        ojlibs::matrix<double> A(M, M);
+        mat_t A(M, M);
         for (int i = 0; i < M; ++i)
             for (int j = 0; j < M; ++j)
                 A(i, j) = dis(gen);
 
-        ojlibs::matrix<double> L, U, LU, PA(M, M);
+        mat_t L, U, LU, PA(M, M);
         vector<int> p;
-        ojlibs::matrix<int> P;
+        mat_t P;
         bool pos;
         ASSERT_EQ(0, PLU_decompose(A, p, L, U, pos));
         LU = L * U;
@@ -127,7 +132,7 @@ TEST(PLU, random) {
                 PA(i, j) = A(p[i], j);
             }
         }
-        P = ojlibs::permutation_matrix<int>(p);
+        P = ojlibs::permutation_matrix<Alg>(p);
 
         for (int i = 0; i < M; ++i)
             for (int j = 0; j < M; ++j)
@@ -143,17 +148,43 @@ TEST(INVERSE, random) {
     std::mt19937 gen;
 
     for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
-        ojlibs::matrix<double> A(M, M);
+        mat_t A(M, M);
         for (int i = 0; i < M; ++i)
             for (int j = 0; j < M; ++j)
                 A(i, j) = dis(gen);
 
-        ojlibs::matrix<double> R = inverse(A);
-        ojlibs::matrix<double> I = A * R;
+        mat_t R = inverse(A);
+        mat_t I = A * R;
 
         for (int i = 0; i < M; ++i)
             for (int j = 0; j < M; ++j)
                 ASSERT_GE(1e-8, abs(I(i, j) - (i == j ? 1.0 : 0.0)));
+
+    }
+}
+
+TEST(INVERSE, random_mod) {
+    constexpr int MOD = 1000000009;
+    using Alg = ojlibs::modular_algebra<MOD>;
+    using mat_t = ojlibs::matrix<Alg>;
+
+    const int M = 50;
+    const int TEST_SIZE = 50;
+    std::uniform_real_distribution<double> dis(0, MOD - 1);
+    std::mt19937 gen;
+
+    for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
+        mat_t A(M, M);
+        for (int i = 0; i < M; ++i)
+            for (int j = 0; j < M; ++j)
+                A(i, j) = dis(gen);
+
+        mat_t R = inverse(A);
+        mat_t I = A * R;
+
+        for (int i = 0; i < M; ++i)
+            for (int j = 0; j < M; ++j)
+                ASSERT_EQ(I(i, j), (i == j ? 1 : 0));
 
     }
 }
