04003e22d31fc605631eab6285da0accaa6894d8
Georeth Chow <georeth2010@gmail.com>
2021-01-02 17:14:20 +0800

    remove unused code

--- inc/ojlibs/util.hpp
+++ inc/ojlibs/util.hpp
@@ -7,11 +7,7 @@ template <typename T>
 bool chmax(T &m, const T &v) { if (v > m) return m = v, true; return false; }
 template <typename T>
 bool chmin(T &m, const T &v) { if (v < m) return m = v, true; return false; }
-template <typename T>
-bool chinc(T &v1, T &v2) { if (v1 > v2) return swap(v1, v2), true; return false; }
-template <typename T>
-bool chdec(T &v1, T &v2) { if (v1 < v2) return swap(v1, v2), true; return false; }
-    
+
 } // namespace ojlibs TO_BE_REMOVED
 
 #endif /* end of include guard: OJLIBS_INC_UTIL_H_ */
