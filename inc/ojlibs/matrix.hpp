9f1f34ef352ddb66c40e5893a295cc518eb0dc02
Georeth Chow <georeth2010@gmail.com>
2021-01-07 20:53:22 +0800

    matrix: PLU_decompose now works on any matrix
    auto plu = PLU_decompose(A); // A can be not square or singular
    auto [p, L, U, pivots] = plu;
    
    then, A(P, :) = L * U
    L is unit lower-triangular
    U is upper-triangular in row echelon form
    (some LA package doesn't ensure echelon, e.g. [[1 0 0],[0,0,2],[0,0,1]])

--- inc/ojlibs/matrix.hpp
+++ inc/ojlibs/matrix.hpp
@@ -1,6 +1,7 @@
 #ifndef OJLIBS_INC_MAT3_H_
 #define OJLIBS_INC_MAT3_H_
 #include <vector>
+#include <utility>
 #include <cmath>
 #include <cassert>
 #include <ojlibs/algebra/base.hpp>
@@ -21,20 +22,23 @@ struct matrix {
     std::vector<T> arr;
     Alg alg;
 
-    explicit matrix(int r = 0, int c = 0, const T &init = 0, const Alg &alg_in = Alg())
-        : nrow(r), ncol(c), alg(alg_in), arr(nrow * ncol, init) { }
+    explicit matrix(int r = 0, int c = 0, const Alg &alg_in = Alg())
+        : nrow(r), ncol(c), arr(r * c, alg_in.zero()), alg(alg_in) { }
     bool operator==(const matrix &that) const {
         assert(nrow == that.nrow && ncol == that.ncol);
         for (int i = 0; i < nrow * ncol; ++i)
             if (arr[i] != that.arr[i]) return false;
         return true;
     }
+    bool operator!=(const matrix &that) const {
+        return !(*this == that);
+    }
     int r() const { return nrow; }
     int c() const { return ncol; }
     T &operator()(int i, int j) { return arr[i * ncol + j]; }
     const T &operator()(int i, int j) const { return arr[i * ncol + j]; }
     static matrix eye(int n, const Alg &alg = Alg()) {
-        matrix mat(n, n);
+        matrix mat(n, n, alg);
         for (int i = 0; i < n; ++i)
             mat(i, i) = alg.one();
         return mat;
@@ -48,7 +52,7 @@ MAT_T operator*(const MAT_T &left, const MAT_T &right) {
     assert(nn == right.r());
 
     auto &alg = left.alg;
-    MAT_T ret(mm, pp);
+    MAT_T ret(mm, pp, alg);
     for (int i = 0; i < mm; ++i) {
         for (int j = 0; j < pp; ++j) {
             auto acc = alg.zero();
@@ -138,7 +142,7 @@ MATRIX_SCALAR_OP_GEN(/, /=, divides)
 TEMPL_ARG
 MAT_T permutation_matrix(const std::vector<int> &p, const Alg &alg = Alg()) {
     int n = (int)p.size();
-    MAT_T P(n, n);
+    MAT_T P(n, n, alg);
     for (int i = 0; i < n; ++i)
         P(i, p[i]) = alg.one();
     return P;
@@ -155,7 +159,7 @@ int LU_decompose(const MAT_T &A, MAT_T &L, MAT_T &U) {
     auto &alg = A.alg;
     assert(n == A.c());
 
-    L = MAT_T::eye(n);
+    L = MAT_T::eye(n, alg);
     U = A;
     for (int j = 0; j < n; ++j) {
         if (U(j, j) == alg.zero())
@@ -172,72 +176,156 @@ int LU_decompose(const MAT_T &A, MAT_T &L, MAT_T &U) {
     return 0;
 }
 
-// return 0 if succeed
-// return -1 if pivots has zero (include the last one)
 // PA = LU
-// P is given by vector p
-//         p[i] = j <=> P[i][j] = 1
+// A[m x n]: input matrix to be decomposed
+// P is permutation matrix given by vector p: p[i] = j <=> P[i][j] = 1
+// L[m x r]: lower triangular, last (m - rank) columns only has 1 on diagonal
+// U[r x n]: upper triangular in row echelon form, last (n - r) rows are zero
+// pivot_index: pivots located at U(p, pivot_index[p]). rank == pivot_index.size()
 TEMPL_ARG
-int PLU_decompose(const MAT_T &A, std::vector<int> &p,
-                  MAT_T &L, MAT_T &U, bool &positive) {
-    int n = A.r();
-    auto &alg = A.alg;
-    assert(n == A.c());
+struct PLU_info {
+    std::vector<int> p;
+    MAT_T L, U;
+    std::vector<int> pivot_index;
+};
 
-    positive = true;
-    U = A;
-    L = MAT_T(n, n);
-    p = std::vector<int>(n);
-    for (int i = 0; i < n; ++i)
+TEMPL_ARG
+PLU_info<Alg> PLU_decompose(const MAT_T &A) {
+    int r = A.r(), c = A.c();
+    auto &alg = A.alg;
+
+    MAT_T U = A;
+    MAT_T L = MAT_T(r, r, alg);
+    std::vector<int> p(r);
+    std::vector<int> pivot_index(r);
+    for (int i = 0; i < r; ++i)
         p[i] = i;
 
-    for (int j = 0; j < n; ++j) {
-        auto max_norm = alg.norm(U(j, j));
-        int max_index = j;
+    int i = 0;
+    for (int j = 0; i < r && j < c; ++j) {
+        auto max_norm = alg.norm(U(i, j));
+        int max_index = i;
 
         // find largest pivot
-        for (int jj = j + 1; jj < n; ++jj) {
-            auto cur_norm = alg.norm(U(jj, j));
+        for (int ii = i + 1; ii < r; ++ii) {
+            auto cur_norm = alg.norm(U(ii, j));
             if (cur_norm > max_norm) {
                 max_norm = cur_norm;
-                max_index = jj;
+                max_index = ii;
             }
         }
         if (U(max_index, j) == alg.zero())
-            return -1;
-        if (j != max_index) {
-            for (int k = 0; k < n; ++k) {
-                std::swap(U(j, k), U(max_index, k));
-                std::swap(L(j, k), L(max_index, k));
-            }
-            std::swap(p[j], p[max_index]);
-            positive = !positive;
+            continue;
+
+        pivot_index[i] = j;
+        if (i != max_index) {
+            for (int k = 0; k < j; ++k)
+                std::swap(L(i, k), L(max_index, k));
+            for (int k = j; k < c; ++k)
+                std::swap(U(i, k), U(max_index, k));
+            std::swap(p[i], p[max_index]);
         }
         // as before
-        for (int i = j + 1; i < n; ++i) {
-            auto lij = alg.divides(U(i, j), U(j, j));
-            L(i, j) = lij;
+        for (int ii = i + 1; ii < r; ++ii) {
+            auto li = alg.divides(U(ii, j), U(i, j));
+            // fprintf(stderr, "set lij %d %d = %d\n", ii, j, lij);
+            L(ii, i) = li;
             // U[i] -= U[j] * lij
-            U(i, j) = 0;
-            for (int k = j + 1; k < n; ++k)
-                U(i, k) = alg.minus(U(i, k), alg.multiplies(U(j, k), lij));
+            U(ii, j) = 0;
+            for (int k = j + 1; k < c; ++k)
+                U(ii, k) = alg.minus(U(ii, k), alg.multiplies(U(i, k), li));
+        }
+        ++i;
+    }
+    pivot_index.resize(i);
+    for (int ii = 0; ii < r; ++ii)
+        L(ii, ii) = 1;
+
+    return PLU_info<Alg>{p, L, U, pivot_index};
+}
+
+TEMPL_ARG
+MAT_T PLU_solve_general(const PLU_info<Alg> &plu) {
+    auto &U = plu.U;
+    auto &pivots = plu.pivot_index;
+    auto &alg = U.alg;
+    MAT_T ret(U.c(), U.c() - (int)pivots.size(), alg);
+
+    int next_pivot = 0;
+    for (int j = 0; j < U.c(); ++j) {
+        if (next_pivot < pivots.size() && pivots[next_pivot] == j)
+            ++next_pivot;
+        else {
+            ret(j, j - next_pivot) = alg.one();
+            for (int p = next_pivot - 1; p >= 0; --p) {
+                auto t = U(p, j);
+                for (int pp = p; pp < next_pivot; ++pp)
+                    t = alg.plus(t, alg.multiplies(ret(pivots[pp], j - next_pivot), U(p, pivots[pp])));
+                ret(pivots[p], j - next_pivot) = alg.negate(alg.divides(t, U(p, pivots[p])));
+            }
         }
     }
-    for (int i = 0; i < n; ++i)
-        L(i, i) = 1;
-    return 0;
+    return ret;
 }
+
+TEMPL_ARG
+std::pair<MAT_T, bool> PLU_solve_special(const PLU_info<Alg> &plu, const MAT_T &y) {
+    assert(y.c() == 1);
+
+    auto &&[p, L, U, pivots] = plu;
+    auto &alg = U.alg;
+    int m = U.r();
+    int n = U.c();
+    int rank = (int)plu.pivot_index.size();
+
+    MAT_T x(n, 1, alg);
+
+    // solve Lz = Py
+    MAT_T z(m, 1, alg);
+    for (int i = 0; i < m; ++i) {
+        auto t = y(p[i], 0);
+        // auto t = py(i, 0);
+        for (int j = 0; j < i; ++j)
+            t = alg.minus(t, alg.multiplies(L(i, j), z(j, 0)));
+        z(i, 0) = t;
+
+        if (i >= rank && t != alg.zero())
+            return {x, false};
+    }
+    // assert(L * z == py);
+
+    // solve Ux = z
+    for (int i = rank - 1; i >= 0; --i) {
+        auto t = z(i, 0);
+        for (int j = i + 1; j < rank; ++j)
+            t = alg.minus(t, alg.multiplies(U(i, pivots[j]), x(pivots[j], 0)));
+
+        int p = pivots[i];
+        x(p, 0) = alg.divides(t, U(i, p));
+    }
+    // assert(U * x == z);
+
+    return {x, true};
+}
+
 
 TEMPL_ARG
 EleType determinant(const MAT_T &A) {
     assert(A.r() == A.c());
     auto &alg = A.alg;
-    std::vector<int> p;
-    bool pos;
-    MAT_T L, U;
 
-    if (PLU_decompose(A, p, L, U, pos) < 0)
+    auto info = PLU_decompose(A);
+    auto &p = info.p;
+    auto &U = info.U;
+    if (info.pivot_index.size() < A.r())
         return alg.zero();
+
+    bool pos = true;
+    for (int i = 0; i < A.r(); ++i)
+        for (int j = 0; j < i; ++j)
+            if (p[j] > p[i])
+                pos = !pos;
+
 
     auto ret = alg.one();
     for (int i = 0; i < A.r(); ++i)
@@ -255,7 +343,7 @@ MAT_T inverse(const MAT_T &A) {
 
     MAT_T U = A;
     int n = U.r();
-    MAT_T R = MAT_T::eye(n);
+    MAT_T R = MAT_T::eye(n, alg);
 
     // first make U upper right
     for (int j = 0; j < n; ++j) {
--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -9,10 +9,10 @@ using namespace std;
 using Alg = ojlibs::base_algebra<double>;
 typedef ojlibs::matrix<Alg> mat_t;
 
-template <typename T, typename Ring>
-void print(const mat_t &m) {
+template <typename Alg>
+void print(const ojlibs::matrix<Alg> &m) {
     for (int i = 0; i < m.r(); ++i) {
-        for (int j = 0; j < m.r(); ++j)
+        for (int j = 0; j < m.c(); ++j)
             cout << m(i, j) << " ";
         cout << endl;
     }
@@ -20,7 +20,7 @@ void print(const mat_t &m) {
 }
 
 TEST(OP, MM) {
-    mat_t m(3, 3, 1.0);
+    mat_t m(3, 3);
     mat_t m2 = m * m;
     m + m;
     m - m;
@@ -93,7 +93,7 @@ TEST(LU, random) {
     const int TEST_SIZE = 200;
     std::uniform_real_distribution<double> dis(-100, 100);
     std::mt19937 gen;
-    
+
     for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
         mat_t A(M, M);
         for (int i = 0; i < M; ++i)
@@ -109,58 +109,149 @@ TEST(LU, random) {
     }
 
 }
+
 TEST(PLU, random) {
     const int M = 50;
     const int TEST_SIZE = 200;
     std::uniform_real_distribution<double> dis(-100, 100);
     std::mt19937 gen;
-    
+
     for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
         mat_t A(M, M);
         for (int i = 0; i < M; ++i)
             for (int j = 0; j < M; ++j)
                 A(i, j) = dis(gen);
 
-        mat_t L, U, LU, PA(M, M);
-        vector<int> p;
-        mat_t P;
+        mat_t LU, PA(M, M);
         bool pos;
-        ASSERT_EQ(0, PLU_decompose(A, p, L, U, pos));
+        auto [p, L, U, pivots] = PLU_decompose(A);
+        ASSERT_EQ(pivots.size(), M);
         LU = L * U;
         for (int i = 0; i < M; ++i) {
             for (int j = 0; j < M; ++j) {
                 PA(i, j) = A(p[i], j);
             }
         }
-        P = ojlibs::permutation_matrix<Alg>(p);
 
         for (int i = 0; i < M; ++i)
-            for (int j = 0; j < M; ++j)
+            for (int j = 0; j < M; ++j) {
                 ASSERT_GE(1e-6, abs(PA(i, j) - LU(i, j)));
-        ASSERT_EQ(pos ? 1 : -1, determinant(P));
+            }
     }
 }
 
-TEST(INVERSE, random) {
-    const int M = 50;
-    const int TEST_SIZE = 200;
-    std::uniform_real_distribution<double> dis(-100, 100);
+TEST(PLU, random_mod_singular_and_solve) {
+    constexpr int MOD = 1000000009;
+    using Alg = ojlibs::modular_algebra<MOD>;
+    using mat_t = ojlibs::matrix<Alg>;
+
+    const int M_MAX = 50;
+    const int TEST_SIZE = 100;
+    std::uniform_int_distribution<int> dis_m(1, M_MAX);
+    std::uniform_int_distribution<int> dis(0, MOD - 1);
     std::mt19937 gen;
 
     for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
-        mat_t A(M, M);
+        int M = dis_m(gen);
+        int N = dis_m(gen);
+        // make sure small rank is tested
+        int rank = nr_test % (std::min(M, N) + 1);
+
+        mat_t B(M, rank), C(rank, N);
         for (int i = 0; i < M; ++i)
-            for (int j = 0; j < M; ++j)
-                A(i, j) = dis(gen);
+            for (int j = 0; j < rank; ++j)
+                B(i, j) = dis(gen);
+        for (int i = 0; i < rank; ++i)
+            for (int j = 0; j < N; ++j)
+                C(i, j) = dis(gen);
+        mat_t A = B * C; // rank(A) <= rank
 
-        mat_t R = inverse(A);
-        mat_t I = A * R;
+        mat_t LU, PA(M, N);
+        bool pos;
+        auto info = ojlibs::PLU_decompose(A);
+        auto &&[p, L, U, pivots] = info;
 
-        for (int i = 0; i < M; ++i)
-            for (int j = 0; j < M; ++j)
-                ASSERT_GE(1e-8, abs(I(i, j) - (i == j ? 1.0 : 0.0)));
+        ASSERT_EQ(p.size(), M);
+        ASSERT_EQ(L.r(), M);
+        ASSERT_EQ(L.c(), M);
+        ASSERT_EQ(U.r(), M);
+        ASSERT_EQ(U.c(), N);
+        ASSERT_TRUE(pivots.size() <= rank);
+        rank = pivots.size();
 
+        // check PA=LU
+        LU = L * U;
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                PA(i, j) = A(p[i], j);
+            }
+        }
+        if (PA != LU) {
+            cout << "ps: " << pivots.size() << "\n";
+            cout << "A:\n";
+            print(A);
+            cout << "PA:\n";
+            print(PA);
+            cout << "L:\n";
+            print(L);
+            cout << "U:\n";
+            print(U);
+            cout << "LU:\n";
+            print(LU);
+        }
+        ASSERT_EQ(PA, LU);
+
+        // solve Ax = y
+        auto null_space = ojlibs::PLU_solve_general(info);
+        if (!(A * null_space == mat_t(M, N - rank))) {
+            cout << "ps: " << pivots.size() << "\n";
+            cout << "A:\n";
+            print(A);
+            cout << "PA:\n";
+            print(PA);
+            cout << "L:\n";
+            print(L);
+            cout << "U:\n";
+            print(U);
+            cout << "X:\n";
+            print(null_space);
+            cout << "AX:\n";
+            print(A * null_space);
+        }
+        ASSERT_EQ(A * null_space, mat_t(M, N - rank));
+
+        mat_t x(N, 1);
+        for (int i = 0; i < N; ++i)
+            x(i, 0) = dis(gen);
+
+        mat_t y = A * x;
+
+        auto [x_2, valid] = ojlibs::PLU_solve_special(info, y);
+        ASSERT_TRUE(valid);
+
+        if (A * x_2 != y) {
+            cout << "ps: " << pivots.size() << "\n";
+            cout << "A:\n";
+            print(A);
+            cout << "PA:\n";
+            print(PA);
+            cout << "L:\n";
+            print(L);
+            cout << "U:\n";
+            print(U);
+            cout << "x:\n";
+            print(x);
+            cout << "y = A x:\n";
+            print(A * x);
+            cout << "x_2:\n";
+            print(x_2);
+            cout << "A x_2:\n";
+            print(A * x_2);
+        }
+
+        ASSERT_EQ(A * x_2, y);
     }
+
 }
 
 TEST(INVERSE, random_mod) {
@@ -169,8 +260,8 @@ TEST(INVERSE, random_mod) {
     using mat_t = ojlibs::matrix<Alg>;
 
     const int M = 50;
-    const int TEST_SIZE = 50;
-    std::uniform_real_distribution<double> dis(0, MOD - 1);
+    const int TEST_SIZE = 20;
+    std::uniform_int_distribution<int> dis(0, MOD - 1);
     std::mt19937 gen;
 
     for (int nr_test = 0; nr_test < TEST_SIZE; ++nr_test) {
